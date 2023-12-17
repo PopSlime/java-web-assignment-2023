94c44b7a7cc859c187753a6537b03ff0020fe45f
PopSlime <popslime@qq.com>
2023-12-17 16:28:00 +0800

    feat!: 初始提交

---
+++ src/main/java/cn/edu/scnu/java_web_assignment_2023/Application.java
@@ -0,0 +1,11 @@
+package cn.edu.scnu.java_web_assignment_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
