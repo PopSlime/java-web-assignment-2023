cfe85e09df19cc15070bec63a2cf1b91b1949b06
shinysmilystory <1252273781@qq.com>
2024-01-06 18:18:54 +0800

    feat:

---
+++ src/main/resources/static/js/top.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     $("#rankSelection").click(function() {
+//         window.location.href = "rank.html";
+//     });
+// });
+//
+// $(document).ready(function() {
+//     $("#filmSelection").click(function() {
+//         window.location.href = "show.html";
+//     });
+// });
+// 当点击首页(排行)按钮时，改变iframe的高度，为其下方预留空间
