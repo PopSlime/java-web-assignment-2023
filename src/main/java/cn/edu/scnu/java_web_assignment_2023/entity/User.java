e0ab15053119e273db8057789f520ad600dc1451
hjw <1609863729@qq.com>
2024-01-10 22:01:41 +0800

    fix: User类增加两个注解

--- src/main/java/cn/edu/scnu/java_web_assignment_2023/entity/User.java
+++ src/main/java/cn/edu/scnu/java_web_assignment_2023/entity/User.java
@@ -1,6 +1,11 @@
 package cn.edu.scnu.java_web_assignment_2023.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName(value="user")
 public class User {
+    @TableId
     private String account;
     private String password;
 
