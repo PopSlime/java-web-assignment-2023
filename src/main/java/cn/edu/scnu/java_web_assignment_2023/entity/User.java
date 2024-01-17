be5760f8de24b1acddac11e5760927b9118ec908
PopSlime <popslime@qq.com>
2024-01-17 23:35:12 +0800

    fix: 修复如果注册时账户为空，数据库接口会自动生成非空 ID 的问题

--- src/main/java/cn/edu/scnu/java_web_assignment_2023/entity/User.java
+++ src/main/java/cn/edu/scnu/java_web_assignment_2023/entity/User.java
@@ -1,5 +1,6 @@
 package cn.edu.scnu.java_web_assignment_2023.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -7,7 +8,7 @@ import lombok.Data;
 @Data
 @TableName(value = "user")
 public class User {
-    @TableId
+    @TableId(type = IdType.INPUT)
     private String userId;
     private byte[] password;
     private boolean isVip;
