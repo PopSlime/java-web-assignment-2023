89024e7817c59c7b1cb4b704f6560a51def26919
PopSlime <popslime@qq.com>
2024-06-24 22:51:22 +0800

    feat: 实现浏览器兼容性检测及错误提示

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,13 @@
 'use strict';
 
 $(document).ready(() => {
+    try {
+        document.querySelector(":has(test)");
+    }
+    catch (DOMException) {
+        window.alert($("#msg-browser-incompatible").text());
+    }
+
     $("#account-subscribe").on("click", function (e) {
         e.preventDefault();
         if (window.confirm($("#msg-subscribe-confirm").text())) {
